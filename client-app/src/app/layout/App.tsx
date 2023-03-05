14c76d90b3c203a280651ca39faa62bfd757bd21
Artyom Chegodar <>
2023-03-05 18:27:18 +0300

    added interface activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import './styles.css';
-import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+    const [activities, setActivities] = useState<Activity[]>([]);
 
-  useEffect(() => {
-    axios.get('https://localhost:7115/api/activities')
-    .then(response => {
-      setActivities(response.data);
-    })
-  }, []);
+    useEffect(() => {
+        axios
+            .get<Activity[]>("https://localhost:7115/api/activities")
+            .then((response) => {
+                setActivities(response.data);
+            });
+    }, []);
 
-
-  return (
-    <div className="App">
-      <Header as='h2' icon='users' content='Reactivities'/>
-      <List>
-        {activities.map((activity: any) => (
-          <li key={activity.id}>{activity.title}</li>
-        ))}
-      </List>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header as="h2" icon="users" content="Reactivities" />
+            <List>
+                {activities.map((activity) => (
+                    <li key={activity.id}>{activity.title}</li>
+                ))}
+            </List>
+        </div>
+    );
 }
 
 export default App;
