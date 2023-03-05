c328ba28aa422c29cb4c78b476fae7bc75d7ebc4
Artyom Chegodar <>
2023-03-05 19:01:38 +0300

    added navbar menu

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./styles.css";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
 
 function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
@@ -16,14 +17,16 @@ function App() {
     }, []);
 
     return (
-        <div className="App">
-            <Header as="h2" icon="users" content="Reactivities" />
-            <List>
-                {activities.map((activity) => (
-                    <li key={activity.id}>{activity.title}</li>
-                ))}
-            </List>
-        </div>
+        <>
+            <NavBar/>
+            <Container style={{marginTop: '7em'}}>
+                <List>
+                    {activities.map((activity) => (
+                        <li key={activity.id}>{activity.title}</li>
+                    ))}
+                </List>
+            </Container>
+        </>
     );
 }
 
