0f75da9fe798c24026fb7b0bfed541f6c04e8460
Artyom Chegodar <>
2023-03-05 22:05:45 +0300

    Selecting an activity to view

--- client-app/src/app/activities/form/ActivityForm.tsx
+++ client-app/src/app/activities/form/ActivityForm.tsx
@@ -14,6 +14,14 @@ export default function ActivityForm() {
                 <Form.Input placeholder='Venue' />
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button floated='right' type='button' content='Cancel' />
+                {/* <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
+                <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
+                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
+                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
+                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
+                <Button floated='right' positive type='submit' content='Submit' />
+                <Button onClick={closeForm} floated='right' type='button' content='Cancel' /> */}
             </Form>
         </Segment>
     );
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityDashboard from "../activities/dashboard/ActivityDashboard";
 
 function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
+    const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
     useEffect(() => {
         axios
@@ -17,11 +18,24 @@ function App() {
             });
     }, []);
 
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities.find(x => x.id == id));
+    }
+
+    const handleCancelSelectActivity = () => {
+        setSelectedActivity(undefined);
+    }
+
     return (
         <>
             <NavBar/>
             <Container style={{marginTop: '7em'}}>
-                <ActivityDashboard activities={activities} />
+                <ActivityDashboard 
+                    activities={activities}
+                    selectedActivity={selectedActivity}
+                    cancelSelectActivity={handleCancelSelectActivity}
+                    selectActivity={handleSelectActivity}
+                />
             </Container>
         </>
     );
