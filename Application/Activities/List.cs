c390ee3043eac47859e8b3cc471282e1b8024130
Artyom Chegodar <>
2023-03-05 16:42:39 +0300

    Revert "demanstration how cancelation token works"
    This reverts commit 51dca65224694a2814bdc9ce965c03c6f94918e5.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,6 @@
 ﻿using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence.Db;
 
 namespace Application.Activities;
@@ -13,30 +12,15 @@ public class List
     public class Handler : IRequestHandler<Query, List<Activity>>
     {
         private readonly DataContext _context;
-        private readonly ILogger<List> _logger;
 
-        public Handler(DataContext context, ILogger<List> logger)
+        public Handler(DataContext context)
         {
             _context = context;
-            _logger = logger;
         }
 
         public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
-            try
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    cancellationToken.ThrowIfCancellationRequested();
-                    await Task.Delay(1000, cancellationToken);
-                    _logger.LogInformation($"Task {i} has completed");
-                }
-            }
-            catch (Exception ex) when(ex is TaskCanceledException)
-            {
-                _logger.LogInformation($"Task was canceled");
-            }
-            return await _context.Activities.ToListAsync(cancellationToken);
+            return await _context.Activities.ToListAsync();
         }
     }
 }
