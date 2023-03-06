53978eb314f59f77748bc4294fa2718205d1a5f5
MS-CAS\admin <a.chegodar@softteco.com>
2023-03-06 16:13:18 +0300

    fix for saving date without UTC format

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,9 @@ builder.AddApplicationServices();
 
 var app = builder.Build();
 
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
