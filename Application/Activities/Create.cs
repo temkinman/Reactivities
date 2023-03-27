4b80e6681a6aa81dc23a35526e86806c9454fe6d
Artyom Chegodar <>
2023-03-28 00:15:15 +0300

    4. Updating the create activity handler Section 14

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,8 +1,10 @@
 ﻿
 using Application.Core;
+using Application.Interfaces;
 using Domain.Entities;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence.Db;
 
 namespace Application.Activities;
@@ -24,15 +26,28 @@ public class Create
     public class Handler : IRequestHandler<Command, Result<Unit>> 
     {
         private readonly DataContext _context;
+        private readonly IUserAccessor _userAccessor;
 
-        public Handler(DataContext context)
+        public Handler(DataContext context, IUserAccessor userAccessor)
         {
             _context = context;
+            _userAccessor = userAccessor;
         }
 
         public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
-            _context.Activities.AddAsync(request.Activity);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == _userAccessor.GetUserName());
+
+            var attendee = new ActivityAttendee
+            {
+                AppUser = user,
+                Activity = request.Activity,
+                IsHost = true
+            };
+
+            request.Activity.Attendees.Add(attendee);
+
+            _context.Activities.Add(request.Activity);
             var result = await _context.SaveChangesAsync() > 0;
 
             if (!result) return Result<Unit>.Failure("Failed to create activity");
